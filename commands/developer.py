0fc1a4c6f6d7435ef8ddbd16afeb8424bee7cd91
༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻⁩ <152390226+LowkeyIsHim@users.noreply.github.com>
2026-09-17 06:35:55 +0100

    Update developer message formatting and content

--- commands/developer.py
+++ commands/developer.py
@@ -5,14 +5,13 @@
 from telegram import Update
 from telegram.ext import ContextTypes, CommandHandler
 
-from branding import LINE
+from branding import header
 
 DEVELOPER_TEXT = (
-    f"{LINE}\n"
-    "built by lowkey\n"
-    f"{LINE}\n\n"
+    f"{header('the person behind this')}\n\n"
+    "built by *lowkey*\n\n"
     "telegram: [@Im\\_just\\_l0wkey](https://t.me/Im_just_l0wkey)\n\n"
-    "reach out for bugs, feature ideas, or just to say hi."
+    "_reach out for bugs, feature ideas, or just to say hi._"
 )
 
 
